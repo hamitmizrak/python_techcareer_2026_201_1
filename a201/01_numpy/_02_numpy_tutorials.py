75017ef1fe5d36559282309df859950d5a7637f7
HamitMizrak <hamitmizrak@gmail.com>
2026-03-28 15:26:08 +0300

    - Numpy Liste - Toplu işlemler

--- a201/01_numpy/_02_numpy_tutorials.py
+++ a201/01_numpy/_02_numpy_tutorials.py
@@ -2,3 +2,26 @@ import numpy as np
 
 
 print("Numpy  Sürümü : ", np.__version__)
+
+# Not: Liste benzerdir ancak iç yapı farklıdır
+# NOTTTTT: Numpy array, sayısal işelmler için daha uygundur
+
+# Liste
+normal_liste= [20,10,30,40,50]
+print(normal_liste)
+
+# Numpy Liste
+numpy_liste =np.array([20,10,30,40,50])
+print(numpy_liste)
+
+dizi = np.array([20,10,30,40,50])
+print(dizi[0])  # ilk eleman
+print(dizi[1])  # ikinci eleman
+print(dizi[-1])  # son eleman
+print(dizi[-2])  # son eleman bir önceki
+
+# Toplu işlemler
+print(dizi+15)
+print(dizi*15)
+print(dizi/15)
+
